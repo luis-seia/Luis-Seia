a78021867736f458a7cf31948e8c091b0f3c9e91
Luis Seia <luis.seiax@gmail.com>
2022-04-12 08:02:05 +0200

    Update adivinha.js

--- ex018/adivinha.js
+++ ex018/adivinha.js
@@ -3,13 +3,13 @@
  luis seia
 */
 
-// Todas as variáveis declaradas aqui em cima - fora das funções - são consideradas GLOBAIS e funcionam dentro de qualquer outra função!
+
 let res = document.querySelector('section#result')
-let computador = 0
+let sorteio = 0
 let jogador = 0
 
 function sortear() {
-    let min = 1 // As variáveis declaradas aqui dentro são LOCAIS, e só funcionam dentro da função onde foram declaradas
+    let min = 1
     let max = 100
     let dif = max - min
     let aleatorio = Math.random()
@@ -17,14 +17,14 @@ function sortear() {
 }
 
 function jogar() {
-    jogador = Number(window.prompt('Qual é o seu palpite?'))
-    if (jogador < computador) {
-        res.innerHTML += `<p>Errdado!!!!! Você Escolheu ${jogador}. O número sorteado é <strong>MAIOR</strong>! que ${jogador}. 
+    jogador = Number(window.prompt('Tente advinhar um numero escolhido pelo computador'))
+    if (sorteio < computador) {
+        res.innerHTML += `<p>Errdado!!!!! Você Escolheu ${sorteio}. O número sorteado é <strong>MAIOR</strong>! que ${sorteio}. 
         tente novamente</p>`
-    } else if (jogador > computador) {
-        res.innerHTML += `<p>Errdado!!!!! Você Escolheu ${jogador}. O número sorteado é <strong>MENOR</strong>! que ${jogador}. tente novamente</p>`
-    } else if (jogador == computador) {
-        res.innerHTML += `<p><strong>PARABÉNS!</strong> Boisse Mandinho acertou! o numero sorteado é ${computador}!</p>`
+    } else if (sorteio > computador) {
+        res.innerHTML += `<p>Errdado!!!!! Você Escolheu ${sorteio}. O número sorteado é <strong>MENOR</strong>! que ${sorteio}. tente novamente</p>`
+    } else if (sorteio == computador) {
+        res.innerHTML += `<p><strong>PARABÉNS!</strong> *******acertou! o numero sorteado é ${computador}********</p>`
         document.getElementById('botao').style.visibility = 'hidden'
     }
 }
